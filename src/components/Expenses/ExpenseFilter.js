194ac357572209bf03f53b73473ac5e66212d332
SanghyunKIM <sanghyun0910.kim@samsung.com>
2022-07-20 09:00:02 +0900

    filter 과제 마무리

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,21 +1,24 @@
 import React from "react";
 import './ExpenseFilter.css'
 
-const ExpenseFilter = () => {
+const ExpenseFilter = (props) => {
+    const filterChangeHandler = (event) =>{
+        props.onChangeFilter(event.target.value);
+    };
+
     return (
         <div className="expenses-filter">
-            <label>Filtered by Year</label>
-            <select className="expenses-filter__control">
-                <option value='2022'>2022</option>
-                <option value='2021'>2021</option>
-                <option value='2020'>2020</option>
-                <option value='2019'>2019</option>
-            </select>
-
+            <div className="expenses-filter__control">
+                <label>Filtered by Year</label>
+                <select value={props.selected}onChange={filterChangeHandler}>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
+            </div>
         </div>
     );
-
-
 };
 
 export default ExpenseFilter;
