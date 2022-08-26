298899884973aad5649737dee2564b0676fd4830
sanghyunkim <blusky0910@gmail.com>
2022-08-26 13:42:59 +0900

    [section11] 카드 추가

--- section11-starting-project/src/components/Layout/Header.js
+++ section11-starting-project/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals.jpeg';
 
 import classes from "./Header.module.css";
 import HeaderCartButton from './HeaderCartButton';
